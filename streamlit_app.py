a67d04ad256f71e53ad7546253f646ccfc429d30
20511YUNI <h098s02511@chunchon.gwe.hs.kr>
2025-11-17 09:06:04 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -196,7 +196,7 @@ with col1:
 
 with col2:
     st.write("유튜브 동영상 예제")
-    st.video("https://www.youtube.com/watch?v=cdbUA1s8Pjg")
+    st.video("https://www.youtube.com/watch?v=YvB8jIU1oaI")
 
 # --- 10. 마무리 ---
 st.markdown("---")
